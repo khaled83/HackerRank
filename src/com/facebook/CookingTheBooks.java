3881128d19fb7c90bc438c22459cdb190f549f6f
khaled83 <khaled.abbas83@gmail.com>
2015-01-21 21:53:05 +0800

    Smallest works, largest fails on 0 left most

--- src/com/facebook/CookingTheBooks.java
+++ src/com/facebook/CookingTheBooks.java
@@ -4,6 +4,105 @@ import java.util.Scanner;
 
 public class CookingTheBooks {
 	
+	
+	private static int smallest(int number, boolean is0Allowed)
+	{
+		if(number / 10 == 0)
+			return number;
+		
+		int remaining = number;
+		
+		int smallest = 9;
+		int indexSmallest = 0;
+		
+		int index = 0;
+		
+		do
+		{
+			int curInt = remaining % 10;
+			if( curInt < smallest && ( is0Allowed || curInt != 0 ) )
+			{
+				smallest = curInt;
+				indexSmallest = index;
+			}
+			
+			remaining = remaining / 10;
+			index++;
+		} while( remaining > 0 );
+		
+		index--;
+		
+		int leftMost = (int) ( number / Math.pow(10, index) );
+		
+		if(!is0Allowed)
+		{
+			if(smallest == 0 || smallest == leftMost)
+			{
+				int leftMostMod = (int) ( leftMost * Math.pow(10, index) );
+				System.out.println("recall smallest recursive with " + leftMost);
+				return leftMostMod
+						+ smallest( number % leftMostMod, true );
+			}
+		}
+		
+		int resultSmallest = (int)
+								 ( number 
+									- ( smallest * Math.pow(10, indexSmallest) ) 
+									+ ( leftMost * Math.pow(10, indexSmallest) )
+									- ( leftMost * Math.pow(10, index) )
+									+ ( smallest * Math.pow( 10, index ) ) );
+		
+		return resultSmallest;
+	
+	}
+	
+	private static int largest(int number)
+	{
+		int remaining = number;
+		
+		int largest = 0;
+		int indexLargest = 0;
+		
+		int index = 0;
+		
+		do
+		{
+			int curInt = remaining % 10;
+				
+			if( curInt > largest )
+			{
+				largest = curInt;
+				indexLargest = index;
+			}
+			
+			remaining = remaining / 10;
+			index++;
+		} while( remaining > 0 );
+		
+		index--;
+		
+		int leftMost = (int) ( number / Math.pow(10, index) );
+		System.out.println("largest="+largest+", leftMost="+leftMost+", indexLargest="+indexLargest);
+		
+		if( largest == leftMost)
+		{
+				int leftMostMod = (int) ( leftMost * Math.pow(10, index) ); 
+				System.out.println("recall largest recursive with leftMostMod = " + leftMostMod + ", number=" + number );
+				return leftMostMod
+						+ largest( number % leftMostMod );
+		}
+		
+		int resultLargest = (int)
+							 ( number 
+								- ( largest * Math.pow(10, indexLargest) ) 
+								+ ( leftMost * Math.pow(10, indexLargest) )
+								- ( leftMost * Math.pow(10, index) )
+								+ ( largest * Math.pow( 10, index ) ) );
+		
+		return resultLargest;
+	}
+	
+	
 	public static void main(String[] args) {
 		System.out.println("My CookingTheBooks Application");
 		Scanner sc = new Scanner(System.in);
@@ -11,55 +110,11 @@ public class CookingTheBooks {
 				
 		for( int caseNum = 1; caseNum<=numCases; caseNum++)
 		{
-			
 			int number = sc.nextInt();
-			int remaining = number;
 			
-			int smallest = 9, largest = number % 10;
-			int indexSmallest = 0, indexLargest = 0;
+			int resultSmallest = smallest(number, false);
+			int resultLargest = largest(number);
 			
-			int index = 0;
-			
-			do
-			{
-				int curInt = remaining % 10;
-				if( curInt < smallest && curInt != 0 )
-				{
-					smallest = curInt;
-					indexSmallest = index;
-				}
-					
-				if( curInt > largest )
-				{
-					largest = curInt;
-					indexLargest = index;
-				}
-				
-				remaining = remaining / 10;
-				index++;
-			} while( remaining > 0 );
-			
-			index--;
-			
-			int leftMost = (int) ( number / Math.pow(10, index) );
-			
-			int resultSmallest = smallest == 0
-								 ? number 
-								 : (int)
-									 ( number 
-										- ( smallest * Math.pow(10, indexSmallest) ) 
-										+ ( leftMost * Math.pow(10, indexSmallest) )
-										- ( leftMost * Math.pow(10, index) )
-										+ ( smallest * Math.pow( 10, index ) ) );
-			
-			int resultLargest = largest == 0 
-								? number
-								: (int)
-									 ( number 
-										- ( largest * Math.pow(10, indexLargest) ) 
-										+ ( leftMost * Math.pow(10, indexLargest) )
-										- ( leftMost * Math.pow(10, index) )
-										+ ( largest * Math.pow( 10, index ) ) );
 			
 			System.out.println("Case #" + caseNum + ": " + resultSmallest + " " + resultLargest);
 		}

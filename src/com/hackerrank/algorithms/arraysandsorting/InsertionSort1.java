b70414bbcf72e9179ce6c55b2fb2a57399bebe6c
khaled83 <khaled.abbas83@gmail.com>
2015-02-06 17:58:05 +0800

    InsertionSort

---
+++ src/com/hackerrank/algorithms/arraysandsorting/InsertionSort1.java
@@ -0,0 +1,45 @@
+package com.hackerrank.algorithms.arraysandsorting;
+
+import java.util.Scanner;
+
+public class InsertionSort1 {
+
+public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        int arrSize = sc.nextInt();
+        int[] arr = new int[arrSize];
+        
+        for(int i=0; i<arrSize; i++) {
+            arr[i] = sc.nextInt();
+        }
+        
+        int lastIndx = arrSize-1;
+        int tmp = arr[lastIndx ];
+        
+        int i = lastIndx - 1;
+        for(i=lastIndx-1; i>=0; i--) {
+            if(arr[i] > tmp) {
+                arr[i+1] = arr[i];
+                printArr(arr);
+            }
+            else {
+                break;
+            }
+        } 
+        
+        arr[i+1] = tmp;
+        printArr(arr);
+
+    }
+    
+    private static void printArr(int[] arr) 
+    {
+        for(int i=0; i<arr.length; i++)
+        {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+}
---
+++ src/com/hackerrank/algorithms/arraysandsorting/InsertionSort2.java
@@ -0,0 +1,56 @@
+package com.hackerrank.algorithms.arraysandsorting;
+
+import java.util.Scanner;
+
+public class InsertionSort2 {
+
+public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        int arrSize = sc.nextInt();
+        int[] arr = new int[arrSize];
+        
+        for(int i=0; i<arrSize; i++) {
+            arr[i] = sc.nextInt();
+        }
+        
+        insertSort(arr); 
+        
+    }
+    
+    private static void insertSort(int[] arr)
+    {
+        for(int size=2; size<=arr.length; size++) {
+            insertSort(arr, size);
+            printArr(arr);
+        }
+    }
+    
+    private static void insertSort(int[] arr, int arrSize)
+    {
+        int lastIndx = arrSize-1;
+        int tmp = arr[lastIndx ];
+        
+        int i = lastIndx - 1;
+        for(i=lastIndx-1; i>=0; i--) {
+            if(arr[i] > tmp) {
+                arr[i+1] = arr[i];
+            }
+            else {
+                break;
+            }
+        }
+        
+        arr[i+1] = tmp;
+    }
+    
+    private static void printArr(int[] arr) 
+    {
+        for(int i=0; i<arr.length; i++)
+        {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+}
